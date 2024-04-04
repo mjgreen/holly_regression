e880d1e701a6257c10ce8b56a2cf49474a88bf24
mgreen <mgreen@bournemouth.ac.uk>
2024-04-04 16:33:34 +0100

    trying sum contrasts

--- script.R
+++ script.R
@@ -1,30 +1,52 @@
-library(tidyverse)
-a=read_csv("test.csv")
+suppressPackageStartupMessages(library(tidyverse))
+library(standardize)
+library(effects)
+library(ggeffects)
+library(see)
+
+a=read_csv("holly_qualtrics_subset.csv", show_col_types = F)
+
+a=a %>% mutate(gender=tolower(gender)) %>% 
+  mutate(gender_superset="other") %>% 
+  mutate(gender_superset=ifelse(gender=="cis woman", "cis woman", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="cis man", "cis man", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="trans man", "trans man", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="non-binary,trans man", "non-binary_trans man", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="non-binary", "non-binary", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="trans man", "trans man", gender_superset)) %>% 
+  mutate(gender_superset=ifelse(gender=="trans woman", "trans woman", gender_superset))
+
 b=a %>% 
   mutate(sexuality = tolower(sexuality)) %>% 
-  mutate(
-    sexuality_superset="other"
-  ) %>% 
-  mutate(sexuality_superset=ifelse(
-    sexuality=="bisexual", "bisexual", sexuality_superset
-  )) %>%
-  mutate(sexuality_superset=ifelse(
-    sexuality=="lesbian", "lesbian", sexuality_superset
-  )) %>% 
-  mutate(sexuality_superset=ifelse(
-    sexuality=="gay", "gay", sexuality_superset
-  )) %>% 
-  mutate(sexuality_superset=ifelse(
-    sexuality=="queer", "queer", sexuality_superset
-  )) 
+  mutate(sexuality_superset = "other") %>%
+  mutate(sexuality_superset = ifelse(sexuality == "bisexual", "bisexual", sexuality_superset)) %>%
+  mutate(sexuality_superset = ifelse(sexuality == "lesbian", "lesbian", sexuality_superset)) %>%
+  mutate(sexuality_superset = ifelse(sexuality == "gay", "gay", sexuality_superset)) %>%
+  mutate(sexuality_superset = ifelse(sexuality == "queer", "queer", sexuality_superset)) 
 
-write_csv(b, "holly_data_with_supersets.csv")
+c=b %>%  
+  select(-c(gender, sexuality)) %>% 
+  rename(centrality=identity_centrality) %>% 
+  rename(superiority=identity_superiority) %>% 
+  rename(negative=identity_negative) %>% 
+  rename(gender  = gender_superset) %>% 
+  rename(sexuality = sexuality_superset) %>% 
+  mutate(gender=str_replace_all(gender, pattern=" ", replace="_"))
+
+write_csv(c, "holly_data_with_supersets.csv")
+
 
 d=b %>% 
+  select(-c(gender, sexuality)) %>% 
   mutate(
-    gender_superset=as_factor(gender_superset),
-    sexuality_superset=as_factor(sexuality_superset)
-  ) %>% select(-c(gender,sexuality))
+    gender=as_factor(gender_superset),
+    sexuality=as_factor(sexuality_superset)
+  ) %>% 
+  select(-c(gender_superset, sexuality_superset)) %>% 
+  rename(centrality=identity_centrality) %>% 
+  rename(superiority=identity_superiority) %>% 
+  rename(negative=identity_negative) %>% 
+  mutate(gender=ifelse(gender=="non-binary_trans man", "non-binary-trans-man", gender))
 
 
 
@@ -33,9 +55,34 @@ mod1=lm(data=d,
           anxiety + avoidance + identity_centrality + identity_superiority + identity_negative + 
           gender_superset + sexuality_superset) 
 
-contrasts(d$sexuality_superset) = "contr.sum"
+#named_contr_sum(x, scale = 1, return_contr = TRUE)
+#contrasts(d$sexuality_superset) = "contr.sum"
+contrasts(d$sexuality_superset) = named_contr_sum(d$sexuality_superset)
+#contrasts(d$gender_superset) = "contr.sum"
+contrasts(d$gender_superset) = named_contr_sum(d$gender_superset)
+
 
 mod2=lm(data=d,
         outcome ~ 
           anxiety + avoidance + identity_centrality + identity_superiority + identity_negative + 
           gender_superset + sexuality_superset) 
+
+summary(mod1) %>%  print()
+summary(mod2) %>% print()
+
+plot(predictorEffects(mod1))
+plot(predictorEffects(mod2) )
+
+
+
+mydf <- predict_response(mod1, terms=c("gender_superset"))
+ggplot(mydf, aes(x, predicted)) + stat_summary()
+  
+
+
+mydf=predict_response(mod2, terms=c("anxiety" , "avoidance", "identity_centrality" , "identity_superiority" , "identity_negative",   "gender_superset" , "sexuality_superset"))
+plot(mydf)
+
+
+mydf=predict_response(mod2, terms=c("anxiety" , "avoidance", "identity_centrality" , "identity_superiority" , "identity_negative"))
+plot(mydf)
